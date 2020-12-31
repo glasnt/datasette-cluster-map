192401b28c011deea7d4d44bdd3b50d35b4cb8ef
Katie McLaughlin <katie@glasnt.com>
2020-12-31 21:43:52 +1100

    Ensure new static included in package

--- setup.py
+++ setup.py
@@ -28,7 +28,7 @@ setup(
     version=VERSION,
     packages=["datasette_cluster_map"],
     entry_points={"datasette": ["cluster_map = datasette_cluster_map"]},
-    package_data={"datasette_cluster_map": ["static/datasette-cluster-map.js"]},
+    package_data={"datasette_cluster_map": ["static/datasette-cluster-map.js", "static/leaflet-map-hash.js"]},
     install_requires=["datasette>=0.48"],
     extras_require={"test": ["pytest", "pytest-asyncio", "httpx", "sqlite-utils"]},
     tests_require=["datasette-cluster-map[test]"],
